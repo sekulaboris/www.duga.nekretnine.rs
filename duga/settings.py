7eda3cccb48ac01d2c501b55e1d7f34f4d895796
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-09-12 09:23:04 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -15,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower=="True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
 
 
 # Application definition
