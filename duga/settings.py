d6ba42560a9b6312774fa93da7475a8c8a6982c5
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-09-12 09:39:59 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -15,10 +15,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG", "False").lower=="True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")#.split('')
+DEBUG = os.environ.get("DEBUG", "False").lower()=="True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
 
 
 # Application definition
@@ -75,7 +75,7 @@ DATABASES = {
 }
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse(database_url)
-#DATABASES["default"] = dj_database_url.parse("postgres://ruvim_new_user:L0TdtrOR5k6bGYmepXkWTMcHum9aZqQG@dpg-cju29h7hdsdc7388jsjg-a.singapore-postgres.render.com/ruvim_new")
+# DATABASES["default"] = dj_database_url.parse("postgres://ruvim_new_user:L0TdtrOR5k6bGYmepXkWTMcHum9aZqQG@dpg-cju29h7hdsdc7388jsjg-a.singapore-postgres.render.com/ruvim_new")
 
 
 # Password validation
