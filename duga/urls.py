b539632939f4c06fba9113cdab4fc0f24c9ba642
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-12 20:59:39 +0200

    Update urls.py

--- duga/urls.py
+++ duga/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('kontakt/', include('contact.urls', namespace='contact')),
    # path('sitemap.xml', sitemap, { 'sitemaps' : sitemaps },
    #     name="django.contrib.sitemaps.views.sitemap",),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #if settings.DEBUG:
 #    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
