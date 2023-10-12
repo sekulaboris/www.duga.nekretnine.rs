1fcfa033c572b9f905598936e97bc363de289703
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-12 21:25:46 +0200

    Update urls.py

--- duga/urls.py
+++ duga/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from django.conf import settings
+from django.conf.urls.static import static
 
 #sitemaps = {'posts': PostSiteMap}
 
@@ -13,9 +15,8 @@ urlpatterns = [
     path('kontakt/', include('contact.urls', namespace='contact')),
    # path('sitemap.xml', sitemap, { 'sitemaps' : sitemaps },
    #     name="django.contrib.sitemaps.views.sitemap",),
-]
-if settings.DEBUG
-urlspatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 #if settings.DEBUG:
 #    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
