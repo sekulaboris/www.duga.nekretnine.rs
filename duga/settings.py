c15cd4f593963a168a4b3e035f8806f7ee95479b
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-15 20:36:01 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_DIRS = [
-    BASE_DIR / "blog/static/",
+    BASE_DIR / "static/",
 ]
 STATIC_URL = 'static/'
 
