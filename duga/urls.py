efa8bcc891b263fc27b51a5a8a5a52cf0c282d03
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-16 21:36:36 +0200

    Update urls.py

--- duga/urls.py
+++ duga/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     url(r'', include('django.contrib.staticfiles.urls')),
 ) + urlpatterns
 
