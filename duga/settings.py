4fdac1950f821c77c81013a85e727e1931b9beb3
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-17 22:07:05 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -114,15 +114,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 #STATICFILES_DIRS = [
-#    BASE_DIR / "static/",
+#    BASE_DIR / "/static/",
 #]
 STATIC_ROOT = [
 os.path.join(BASE_DIR, "static")
 ]
 STATIC_URL = 'static/'
 
-#MEDIA_ROOT =  os.path.join(BASE_DIR, 'static') 
-#MEDIA_URL = '/images/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static') 
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
