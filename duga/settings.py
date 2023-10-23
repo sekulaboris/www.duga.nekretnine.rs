1719d443567a5bc3b1fc44d8fda4d117304da3e7
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-23 07:10:24 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -114,14 +114,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_ROOT = [
-#os.path.join(BASE_DIR, "static")
-#]
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = [
+os.path.join(BASE_DIR, "static")
+]
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'static') 
 MEDIA_URL = '/images/'
 
