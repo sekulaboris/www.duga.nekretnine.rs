95aab16c6ff033b4c47c644523da21d7ce9bfa15
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-23 10:50:42 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -79,6 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        #'default':dj_database_url.parse(),    jedan od nacina za povezivanje baze
     }
 }
 database_url = os.environ.get("DATABASE_URL")
