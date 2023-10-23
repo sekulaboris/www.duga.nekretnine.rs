a6f26e860b0b685a74502e8c9561ebfc9e809ee2
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-23 07:42:18 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -122,9 +122,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = [
-os.path.join(BASE_DIR, "static")
-]
+#STATIC_ROOT = [
+3os.path.join(BASE_DIR, "static")
+#]
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'static') 
 MEDIA_URL = '/images/'
 
