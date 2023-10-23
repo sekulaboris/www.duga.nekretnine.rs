d2fe889925995e09b47f452b59dbb19ebeaf3264
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-23 07:29:35 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 
 # Quick-start development settings - unsuitable for production
