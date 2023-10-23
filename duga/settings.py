1707d19db7eefc2f8e689703d451ffa8625e80f4
sekulaboris <122439224+sekulaboris@users.noreply.github.com>
2023-10-23 07:49:57 +0200

    Update settings.py

--- duga/settings.py
+++ duga/settings.py
@@ -70,12 +70,12 @@ WSGI_APPLICATION = 'duga.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 database_url = os.environ.get("DATABASE_URL")
